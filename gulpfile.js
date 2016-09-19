2175790b6e7be9f3e029fab5c9e21ec6d6f27d2a
CristhGunners <gunners_one@hotmail.com>
2016-09-18 23:34:09 -0300

    New gulp setting

--- gulpfile.js
+++ gulpfile.js
@@ -1,82 +1,98 @@
 var gulp = require('gulp');
 
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
 var jade = require('gulp-jade');
 
 var stylus = require('gulp-stylus');
 var nib = require('nib');
 
+var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
-
-var autoprefixer = require('autoprefixer-stylus');
-
-var concat = require('gulp-concat');
+var imagemin = require('gulp-imagemin');
 
 // Paths
-
 var paths = {
-    stylusEntry: './source/stylus/style.styl',
-    stylusAll: './source/stylus/**/*.styl',
-    jadeEntry: './source/template/*.jade',
-    jadeAll: './source/templates/**/*.jade',
-    jsAll: './source/scripts/**/*.js',
-    jsEntry: './source/scripts/main.js',
-    css: './site/css',
-    js: './site/js',
-    html: './site'
+  jadeEntry: './source/template/index.jade',
+  jadePages: './source/templates/*.jade',
+  jadeComponents: './source/templates/**/*.jade',
+  stylusEntry: './source/stylus/style.styl',
+  stylusComponents: './source/stylus/**/*.styl',
+  jsEntry: './source/scripts/app.js',
+  jsFiles: './source/scripts/**/*.js',
+  imagesSource: './source/images/*',
+  fontsSource: './source/fonts/*',
+  iconsSource: './source/icons/**',
+  fonts: './site/fonts',
+  icons: './site/icons',
+  images: './site/images',
+  css: './site/css',
+  js: './site/js',
+  html: './site'
 };
-
-// Concat Js
-
-gulp.task('scripts', function() {
-  return gulp.src([paths.jsAll])
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest(paths.js));
-});
-
-
-// Javascripts Compress
-gulp.task('compressjs', function() {
-  return gulp.src([paths.jsEntry,paths.jsAll])
-    .pipe(uglify())
-    .pipe(reload({stream: true}))
-    .pipe(gulp.dest(paths.js));
-});
 
 // Jade Compile
 gulp.task('jade2html', function() {
-  gulp.src([paths.jadeEntry,paths.jadeAll])
-    .pipe(jade({
-      pretty: false
-    }))
-    .pipe(reload({stream: true}))
-    .pipe(gulp.dest(paths.html));
+  gulp.src([paths.jadeEntry, paths.jadePages])
+  .pipe(jade({
+    pretty: false
+  }))
+  .pipe(reload({stream: true}))
+  .pipe(gulp.dest(paths.html));
 });
 
 // Stylus Compile
 gulp.task('stylus2css', function () {
   gulp.src([paths.stylusEntry])
-    .pipe(stylus({
-        use: [nib(), autoprefixer()],
-        compress:true,
-        'include css': true,
-    }))
-    .pipe(reload({stream: true}))
-    .pipe(gulp.dest(paths.css));
+  .pipe(stylus({
+    use: [nib()],
+    import: ['nib'],
+    compress:false,
+    'include css': true,
+  }))
+  .pipe(reload({stream: true}))
+  .pipe(gulp.dest(paths.css));
 });
 
-// Watch Files
-gulp.task('serve', ['stylus2css', 'jade2html', 'compressjs'],function() {
+// Javascripts Concat + Compress
+gulp.task('scripts', function() {
+  return gulp.src([paths.jsEntry, paths.jsFiles])
+  .pipe(concat('app.js'))
+  .pipe(uglify())
+  .pipe(reload({stream: true}))
+  .pipe(gulp.dest(paths.js));
+});
+
+// Minify Images
+gulp.task('minimages', function(){
+  gulp.src(paths.imagesSource)
+  .pipe(imagemin())
+  .pipe(gulp.dest(paths.images));
+});
+
+// Move Font & Icon Folder
+gulp.task('movefolders', [], function() {
+  gulp.src(paths.fontsSource).pipe(gulp.dest(paths.fonts));
+  gulp.src(paths.iconsSource).pipe(gulp.dest(paths.icons));
+});
+
+// Broser Sync
+gulp.task('browser-sync', [], function() {
   browserSync({
     server: {
       baseDir: './site'
     },
     open: false
   });
-  gulp.watch([paths.jsEntry,paths.jsAll], ['compressjs']);
-  //gulp.watch(paths.jsEntry, ['scripts']);
-  gulp.watch([paths.stylusEntry,paths.stylusAll], ['stylus2css']);
-  gulp.watch([paths.jadeEntry,paths.jadeAll], ['jade2html']);
+});
+
+// Watch Files
+gulp.task('serve', ['jade2html', 'stylus2css', 'scripts', 'minimages', 'movefolders', 'browser-sync'],function() {
+  gulp.watch([paths.jadeEntry,paths.jadePages, paths.jadeComponents], ['jade2html'], reload);
+  gulp.watch([paths.stylusEntry,paths.stylusComponents], ['stylus2css'], reload);
+  gulp.watch([paths.jsEntry,paths.jsFiles], ['scripts'], reload);
+  gulp.watch([paths.imagesSource], ['minimages'], reload);
+  gulp.watch([paths.fontsSource, paths.iconsSource], ['movefolders'], reload);
 });
